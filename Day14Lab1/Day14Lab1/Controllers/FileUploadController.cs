a811c789c8c22874f1213ed251f7f66bcb763365
Marco Maier <marcomaier3903@gmail.com>
2023-03-27 16:47:01 +0200

    Upload File

---
+++ Day14Lab1/Day14Lab1/Controllers/FileUploadController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day14Lab1.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UploadFile()
+        {
+            if(Request.Form.Files.Count > 0)
+            {
+                foreach(var file in Request.Form.Files)
+                {
+                    var fileName = file.FileName;
+                    using(var outputFile = System.IO.File.Create($"UploadFiles/{fileName}"))
+                    {
+                        file.CopyTo(outputFile);
+                    }
+                }
+            }
+            return Ok("Saved!");
+        }
+    }
+}
