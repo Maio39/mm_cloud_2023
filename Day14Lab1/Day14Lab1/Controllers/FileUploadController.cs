37bdffa0dfb91e41fc9e41dcda886a7729dfcb71
Marco Maier <marcomaier3903@gmail.com>
2023-03-28 09:08:26 +0200

    Add Logo in the Image

--- Day14Lab1/Day14Lab1/Controllers/FileUploadController.cs
+++ Day14Lab1/Day14Lab1/Controllers/FileUploadController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
 
 namespace Day14Lab1.Controllers
 {
@@ -36,8 +37,31 @@ namespace Day14Lab1.Controllers
                 return NotFound();
             }
             var rawData = System.IO.File.ReadAllBytes(fullname);
-            //we FIX this is an IMAGE
+            Bitmap bitmap = new Bitmap(fullname);
+            Graphics graphic = Graphics.FromImage(bitmap);
+
+            string myWater = $"MMLogo {DateTime.Now}";
+            Font font = new Font(FontFamily.GenericSansSerif,20,FontStyle.Bold);
+            Size l_w = graphic.MeasureString(myWater, font).ToSize();
+            Point p = new Point(10,20);
+            Point p1 = new Point(11, 21);
+
+            //Background
+            graphic.FillRectangle(new SolidBrush(Color.Aquamarine), new Rectangle(p, l_w));
+            //Subpixeling
+            graphic.DrawString(myWater, font, Brushes.Black, p1);
+            //
+            graphic.DrawString(myWater, font, Brushes.White, p);
+
+            using(MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms,System.Drawing.Imaging.ImageFormat.Png);
+                return File(ms.ToArray(),"image/png");
+            }
+
+            /*/we FIX this is an IMAGE
             return File(rawData, "image/jpg");
+            //*/
         }
     }
 }
