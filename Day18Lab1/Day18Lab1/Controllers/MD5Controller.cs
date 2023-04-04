f7f01b5e7159f06d880ef8d9d1b9a8722be0e4d2
Marco Maier <marcomaier3903@gmail.com>
2023-04-04 09:49:24 +0200

    Add Project and MD5Controller

---
+++ Day18Lab1/Day18Lab1/Controllers/MD5Controller.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace Day18Lab1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MD5Controller : ControllerBase
+    {
+        public MD5Controller() { }
+
+        private string Compute(string PlainText)
+        {
+            var md5 = MD5.Create();
+            byte[] HashCode = md5.ComputeHash(System.Text.UTF8Encoding.UTF8.GetBytes(PlainText));
+            return BitConverter.ToString(HashCode);
+        }
+        [HttpGet]
+        public IActionResult Get(string plainText)
+        {
+            return Ok(Compute(plainText));
+        }
+
+        [HttpPost]
+        public IActionResult Validate(string PlainText, string HexText)
+        {
+            if(HexText.Equals(Compute(PlainText)))
+            {
+                return Ok(true);
+            } else
+            {
+                return Ok(false);
+            }
+            //return BadRequest();
+        }
+    }
+
+    
+}
