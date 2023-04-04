5db801ada744982fe2bfb46def9976cf2fd1237b
Marco Maier <marcomaier3903@gmail.com>
2023-04-04 11:59:44 +0200

    Encode Md5 by client web app interface

---
+++ Day18Lab1/Day18Lab2/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Day18Lab2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Day18Lab2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly HttpClient _httpClient;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory ApiFactory)
+        {
+            _logger = logger;
+            //Uso
+            _httpClient=ApiFactory.CreateClient("Md5");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> query(string PlainText) 
+        {
+            //To itbase + ?PlainText=ciccio
+            string url = $"{_httpClient.BaseAddress.ToString()}?PlainText={PlainText}";
+            var res = await _httpClient.GetAsync(url);
+            res.EnsureSuccessStatusCode();
+            if(res.IsSuccessStatusCode)
+            {
+                var dato = await res.Content.ReadAsStringAsync();
+                ViewData["result"]=dato.ToString();
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Day18Lab1/Day18Lab2/Program.cs
@@ -0,0 +1,59 @@
+using Day18Lab2.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+string uri = builder.Configuration["WebMD5:uri"];
+//
+builder.Services.AddHttpClient("Md5", client =>
+{
+    client.BaseAddress = new Uri(uri);
+    client.DefaultRequestHeaders.Add("mytoken", "antani");
+});
+/*/
+string caturi = builder.Configuration["cats:uri"];
+builder.Services.AddHttpClient("cats", client =>
+{
+    client.BaseAddress = new Uri(caturi);
+    client.DefaultRequestHeaders.Add("mytoken", "antani");
+});
+//*/
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
