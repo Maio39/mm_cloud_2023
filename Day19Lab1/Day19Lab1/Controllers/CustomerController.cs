650ea7848674161e39eeb286bea4261dfa01bd11
Marco Maier <marcomaier3903@gmail.com>
2023-04-06 10:06:34 +0200

    Initial Day19Lab1

---
+++ Day19Lab1/Day19Lab1/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using Day19Lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day19Lab1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly DataContext _context;
+        public CustomerController(DataContext _DContext) 
+        {
+            _context = _DContext;
+            //Popolate DB
+            if(_context.Customers.Count()==0) { 
+                for(int i=0;i<10;i++)
+                {
+                    Customer mio = new Customer()
+                    {
+                        CustomerStatus = 1,
+                        CustomerName = $"Customer {i}",
+                        CustomerEmail = $"cust{i}@test.com",
+                        CustomerPhone = $"555-5{i}9-9338"
+                    };
+                    _context.Customers.Add(mio);
+                }
+                _context.SaveChanges();
+            }
+        }
+        public IActionResult Index()
+        {
+            var custs = _context.Customers.ToList();
+            return View(custs);
+        }
+    }
+}
---
+++ Day19Lab1/Day19Lab1/Models/Models.cs
@@ -0,0 +1,12 @@
+﻿namespace Day19Lab1.Models
+{
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerEmail { get; set; }
+        public string CustomerPhone { get; set; }
+        public int CustomerStatus { get; set; } 
+
+    }
+}
